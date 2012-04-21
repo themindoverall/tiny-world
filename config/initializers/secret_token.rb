80d6bfdd76085c7992bebd294209358bfbcc9b96
Chris Serino <themindoverall@gmail.com>
2012-04-21 00:36:20 -0400

    Added rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TinyWorld::Application.config.secret_token = '18d5f5ddd1a9ba9e3c43847e8172fec08812a1bd63f7193e24c4804c9c0f4ddcc96505d137fb79a8f8de2cd264d1c0f217fcbf4a6f52b629510751f62ebd5735'
